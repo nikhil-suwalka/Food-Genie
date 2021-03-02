d0af786e31b28b5406161ff444f93690d3d9a924
Nishit Shanbhag <nishit.shanbhag@gmail.com>
2021-03-02 22:16:34 +0530

    Implemented relationships

--- scrapy_app/scrapy_app/pipelines.py
+++ scrapy_app/scrapy_app/pipelines.py
@@ -18,13 +18,12 @@ def getConnection():
 
 
 def checkRecipe(tx, link):
-    print("LINK", link)
-    result = tx.run("MATCH (n:Recipe) where n.link = $link return n.name as name",link=link)
-    return [r['name'] for r in result]
+    result = tx.run("MATCH (n:Recipe) where n.link = $link return ID(n) as id",link=link)
+    return [r['id'] for r in result]
 
 
 def createRecipe(tx, item):
-    tx.run("CREATE (n:Recipe {name: $name,"
+    id = tx.run("CREATE (n:Recipe {name: $name,"
            "details:$details, " 
            "ingredients:$ingredients," 
            "calories:$calories," 
@@ -33,7 +32,7 @@ def createRecipe(tx, item):
            "preparation_time:$preparation_time, " 
            "cooking_time:$cooking_time, " 
            "total_time:$total_time, " 
-           "link:$link})",
+           "link:$link}) return n.identity as id",
            name=item["title"],
            details=item["details"],
            ingredients=item["ingredients"],
@@ -47,35 +46,44 @@ def createRecipe(tx, item):
 
 
 def checkIngredient(tx, ing):
-    result = tx.run("MATCH (i:Ingredient {name: $name}) RETURN i.name as name", name=ing)
-    return [r['name'] for r in result]
+    result = tx.run("MATCH (i:Ingredient {name: $name}) RETURN ID(i) as id", name=ing)
+    return [r['id'] for r in result]
 
 
 def createIngredient(tx, ing):
-    tx.run("CREATE (n:Ingredient {name: $name})", name=ing)
+    result = tx.run("CREATE (n:Ingredient {name: $name}) return ID(n) as id", name=ing)
 
 def createRelationship(tx,rid,iid):
-    tx.run("MATCH (a:Recipe), (b:Ingredient) WHERE a.identity = $rid AND b.identity = $idd CREATE (a)-[r:RELTYPE]->(b) RETURN type(r)", rid=rid, iid=iid)
+    tx.run("MATCH (a:Recipe), (b:Ingredient) WHERE ID(a) = $rid AND ID(b) = $iid CREATE (a)-[r:CONTAINS]->(b)", rid=rid, iid=iid)
 
 
 def addNewRecipe(conn, item):
-    results = []
     session = conn.session()
     recipes = session.read_transaction(checkRecipe, item["link"])
-    if len(results) == 0:
+    if len(recipes) == 0:
         # create ingredients if not present
+        iids = []
+
         for ing in item['ingredients']:
             result = session.read_transaction(checkIngredient, ing)
 
             if len(result) == 0:
                 session.write_transaction(createIngredient, ing)
-                print("Created", ing)
+                print(session.read_transaction(checkIngredient,ing), "TEST")
+                iid = session.read_transaction(checkIngredient,ing)[0]
 
+                iids.append(iid)
+                print("Created", ing)
+            else:
+                iids.append(result[0])
         # create recipe
         session.write_transaction(createRecipe, item)
+        rid = session.read_transaction(checkRecipe,item['link'])[0]
         print("Created", item["title"])
         # create relation
-
+        print("IDS",iids,rid)
+        for iid in iids:
+            session.write_transaction(createRelationship, rid, iid)
 
 class ScrapyAppPipeline:
     def process_item(self, item, spider):
