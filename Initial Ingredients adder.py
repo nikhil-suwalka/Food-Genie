43117eae86bb3385b541a01a4c8eeb6a3d11f88c
Nikhil Suwalka <suwalka18@gmail.com>
2021-03-03 15:21:09 +0530

    Added ingredients from CSV

---
+++ Initial Ingredients adder.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from scrapy_app.scrapy_app.pipelines import *
+
+data = pd.read_csv("RAW_recipes.csv")
+import json
+
+error = 0
+unique_ingredients = set()
+for i in range(len(data)):
+    try:
+        d = data.iloc[i, 0].replace("'", "\"")
+        j = json.loads(d)
+        for ingr in j:
+            unique_ingredients.add(ingr)
+    except:
+        pass
+print("Length: ", len(unique_ingredients))
+print(unique_ingredients)
+
+conn = getConnection()
+session = conn.session()
+
+for ingr in unique_ingredients:
+    session.write_transaction(createIngredient, ingr)
+conn.close()
